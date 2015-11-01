829066746482469fc7cfe745c8bf5a30d0eb29e0
michael <hirsch617@gmail.com>
2015-10-31 23:47:01 -0400

    first pass, functionalize, code reuse

--- rees.py
+++ rees.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
-from __future__ import division
-from os.path import expanduser
-from numpy import logspace, loadtxt,tile
-from dateutil.parser import parse
-import h5py
+from __future__ import division,absolute_import
+from numpy import tile,ones_like
+from matplotlib.pyplot import show
+import seaborn as sns
+sns.color_palette(sns.color_palette("cubehelix"))
+sns.set(context='talk', style='whitegrid',
+        rc={'image.cmap': 'cubehelix_r'}) #for contour
 #
-from gridaurora.ztanh import setupz
-from msise00.demo_msis import rungtd1d
-from rees_model import ionization_profile_from_flux
+from rees_model import reesiono,loadaltenergrid,plotA
+from gridaurora.writeeigen import writeeigen
 
-isotropic=False
 """
-Discussion:
-We need to know how incident flux from the magnetosphere ionizes various species
-vs. altitude and inital beam energy in the ionosphere. The electron production rate q
-may be related to auroral optical intensity via known proportions.
+Models unit incident flux from the magnetosphere ionizing N2,O,O2
+vs. altitude and inital beam energy in the ionosphere.
+The electron production rate q is related to auroral optical intensity via
+known proportions.
 
-Variables defined:
+Variables:
 q: e- production rate [cm^-3 s^-1]
-Phi: "flux" [cm^-2 s^-1 eV^-1]
+Phi: "flux" [cm^-2 s^-1 eV^-1] (here )
 A: energy deposition matrix [eV cm^-1]
 
 References:
@@ -26,66 +26,29 @@ Rees 1989
 Wedlund et al "Electron Energy Spectra and Auroral Arcs" JGR 2013
 Sergienko and Ivanov 1993 "A new approach to calculate the excitation of atmospheric gases by auroral electron impact"
 """
-def reesmodel(dtime,altkm,E,glat,glon,f107a,f107,ap,mass,matfn,h5fn):
 
-    dens,temp = rungtd1d(dtime,altkm,glat,glon,f107a,f107,ap,mass)
-    rho = dens['Total'].values
-    #%% Wedlund 2013 Eqn 4
+def runrees(t,glat,glon,f107a,f107,ap,mass,isotropic,outfn,minalt,nalt,vlim):
     """
-    each ij-th element of matrix A is defined in Eqn. 4
+    inputs:
+    t: time(s) to model, datetime() or ut1_unix
+
+
     """
-    for i in range(altkm.size):
-        for j,e in enumerate(E):
-            A[i,j] = ( Lambda(s[i]/R[j]) * rho[i] * e *(1-albedo[j]) * dE[j] ) / ( R[e] * W[e])
-    #%% Wedlund 2013 Eqn 1-3 (discretized form)
-    q = A.dot(Phi)
+    altkm,E = loadaltenergrid(minalt,nalt)
 
-def rees_aida(dtime,altkm,E,glat,glon,f107a,f107,ap,mass):
-    dens,temp = rungtd1d(dtime,altkm,glat,glon,f107a,f107,ap,mass)
-#%% temporary for testing with octave
-#    from scipy.io import savemat
-#    savemat('debug_msis.mat',{'z':altkm,'E':E,'O':dens['O'].values,
-#                              'N2':dens['N2'].values,'O2':dens['O2'].values,
-#                              'Total':dens['Total'].values},
-#            oned_as='column')
-#%% python port
-    return ionization_profile_from_flux(E,dens,isotropic)
+    Aaida = reesiono(t, altkm, E, glat, glon, f107a, f107, ap, mass,isotropic)
 
-def plotA(A,z,E,ttxt):
-    fg = figure()
-    ax = fg.gca()
-    hi = ax.imshow(A,extent=(E[0],E[-1],z[0],z[-1]),aspect='auto',origin='lower',
-                                        interpolation='none')#,norm=LogNorm())
-    fg.colorbar(hi,ax=ax)
-    ax.set_title(ttxt)
+#%% outputs
+    writeeigen(outfn,E,t,tezs=None,latlon=(glat,glon))
 
-    ax = figure().gca()
-    ax.plot(A,z)
-    ax.set_title(ttxt)
+    plotA(Aaida,altkm,E,'Rees deposition matrix',vlim)
 
-def writeA(A,z,E,dtime,ofn):
-    with h5py.File(expanduser(ofn),'w',libver='latest') as f:
-        f['/A']=A
-        f['/z']=z
-        f['/E'] =E
-        f['/time']=str(dtime)
-        f['/glat']=glat
-        f['/glon']=glon
+
 
 if __name__ == '__main__':
-    from matplotlib.pyplot import figure,show
-    #from matplotlib.colors import LogNorm
-
-    import sys; sys.path.append('../transcarutils') #FIXME this approach is to avoid deeply nested transcarutils dependencies
-    from readionoinit import getaltgrid #transcarutils
-    import seaborn as sns
-    sns.color_palette(sns.color_palette("cubehelix"))
-    sns.set(context='talk', style='whitegrid',
-            rc={'image.cmap': 'cubehelix_r'}) #for contour
-#
     from argparse import ArgumentParser
     p = ArgumentParser(description='rees model of excitation rates for the aurora')
-    p.add_argument('simtime',help='yyyy-mm-ddTHH:MM:SSZ time of sim',type=str,nargs='?',default='2013-03-31T09:00:00Z')
+    p.add_argument('simtime',help='yyyy-mm-ddTHH:MM:SSZ time of sim',nargs='?',default='2013-03-31T09:00:00Z')
     p.add_argument('-c','--latlon',help='geodetic latitude/longitude (deg)',type=float,nargs=2,default=(65.12,-147.43 ))
     p.add_argument('--f107a',help=' 81 day AVERAGE OF F10.7 FLUX (centered on day DDD)',type=float,default=107.6)
     p.add_argument('--f107',help='DAILY F10.7 FLUX FOR PREVIOUS DAY',type=float,default=126.0)
@@ -93,41 +56,15 @@ if __name__ == '__main__':
     p.add_argument('--mass',help=('MASS NUMBER (ONLY DENSITY FOR SELECTED GAS IS ' +
                        'CALCULATED.  MASS 0 IS TEMPERATURE.  MASS 48 FOR ALL. '+
                          'MASS 17 IS Anomalous O ONLY.'),type=float,default=48)
-    p.add_argument('--loadA',help='filename from already computed Rees A',type=str,default=None)
-    p.add_argument('--loadE',help='loads E from transcar input',type=str,default='~/code/transcar/dir.transcar.server/BT_E1E2prev.csv')
-    p.add_argument('--loadZ',help='loads Z from transcar input',type=str,default='~/code/transcar/dir.transcar.server/dir.input/conttanh.dat')
-    p.add_argument('--h5',help='saves deposition matrix A as HDF5',type=str,default=None)
+    p.add_argument('--minalt',help='minimum altitude in grid [km]',type=float,default=30)
+    p.add_argument('--nalt',help='Number of points in altitude grid',type=int,default=286)
+    p.add_argument('-o','--outfn',help='give hdf5 filename to save eigenprofile production')
+    p.add_argument('--isotropic',help='isotropic or non-isotropic pitch angle',action='store_true')
+    p.add_argument('--vlim',help='plotting limits on energy dep and productio plots',nargs=2,type=float,default=(1e-7,1e1))
     p = p.parse_args()
 
-    dtime = parse(p.simtime)
-#%%
-    if p.loadZ is not None:
-        altkm = getaltgrid(expanduser(p.loadZ))
-    else:
-        altkm = setupz(286,90,1.5,11.1475)
-    altkm = altkm[altkm<=1000] #keeps original spacing, but only auroral altitudes
-#%% energy of beams
-    if p.loadE is not None:
-        E = loadtxt(expanduser(p.loadE),usecols=[0],delimiter=',')
-    else:
-        E = logspace(1.72,4.25,num=33,base=10)
-#%%
-    glat,glon = p.latlon
-#%% from AIDA B. Gustavsson
-    if p.loadA is not None:
-        with h5py.File(expanduser(p.loadA),'r',libver='latest') as f:
-            Aaida = f['/A'].value; zaida=f['/z'].value; Eaida=f['/E'].value
-        plotA(Aaida, zaida, Eaida,'Rees deposition matrix')
-    else: #compute A
-        Aaida = rees_aida(dtime, altkm, E, glat, glon, p.f107a, p.f107, p.ap, p.mass)
-        if p.h5:
-            writeA(Aaida,altkm,E,dtime,p.h5)
-        plotA(Aaida,altkm,E,'Rees deposition matrix')
-#%% load JGR2013 data
-    with h5py.File('../transcarutils/precompute/01Mar2011_FA.h5','r',libver='latest') as f:
-        Ajgr = f['/Mp'].value; zjgr = f['/z'].value; Ejgr=f['/E'].value
-    plotA(Ajgr.sum(axis=0),zjgr,Ejgr,'JGR2013 deposition matrix')
-#%%
-    #reesmodel(dtime, altkm, E, glat, glon, p.f107a, p.f107, p.ap, p.mass)
+    runrees(p.simtime,p.latlon[0],p.latlon[1],
+            p.f107a,p.f107,p.ap,p.mass,p.isotropic,
+            p.outfn,p.minalt,p.nalt,p.vlim)
 
     show()
--- rees_model.py
+++ rees_model.py
@@ -4,13 +4,29 @@
    After Sergienko and Ivanov 1993
    a massively speeded up implementation after the AIDA_TOOLS package by Gustavsson, Brandstrom, et al
 """
-from __future__ import division
+from __future__ import division,absolute_import
+from six import string_types
 from pandas import DataFrame
-from numpy import gradient,array,linspace,zeros,diff,append,empty,arange,log10,exp,nan
+from numpy import (gradient,array,linspace,zeros,diff,append,empty,arange,log10,exp,nan,
+                   logspace)
 from scipy.interpolate import interp1d
+from matplotlib.pyplot import figure
+from matplotlib.colors import LogNorm
+from matplotlib.ticker import MultipleLocator
+#
+from gridaurora.ztanh import setupz
+from msise00.runmsis import rungtd1d
+try:
+    from glowaurora.runglow import glowalt
+except:
+    pass
 
 #TODO check that "isotropic" is handled consistently with original code
 def ionization_profile_from_flux(E,dens,isotropic):
+    """
+    simple model for volume emission as function of altitude.
+    After Sergienko and Ivanov 1993 and Gustavsson AIDA_TOOLs
+    """
     E_cost_ion = array([36.8,26.8,28.2])
     ki = array([1, 0.7, 0.4])
 
@@ -19,18 +35,21 @@ def ionization_profile_from_flux(E,dens,isotropic):
     Partitioning = partition(dens,ki)
 
 #%% First calculate the energy deposition as a function of altitude
-    qall = empty((dens.shape[0],E.size))
+    qE = empty((dens.shape[0],E.size)) # Nalt x Nenergy
     for i,(e,d) in enumerate(zip(E,dE)):
         Ebins = linspace(e,e+d,20)
-        #for both isotropic and field aligned electron beams
+        #for isotropic or field aligned electron beams
         Am = energy_deg(Ebins,isotropic,dens)
 
-        q= Am.sum(axis=0)
-        q *= (Partitioning/E_cost_ion).sum(axis=1)
-        qall[:,i] = q
-    return qall
+        q= Am.sum(axis=0) #sum over the interim energy sub-bins
+        q *= (Partitioning/E_cost_ion).sum(axis=1) #effect of ion chemistry at each altitude
+        qE[:,i] = q
+    return qE
 
 def energy_deg(E,isotropic,dens):
+    """
+    energy degradation of precipitating electrons
+    """
     atmp = DataFrame(index=dens.index)
     atmp[['N2','O','O2']] = dens[['N2','O','O2']]/1e6
     atmp['Total'] = dens['Total']/1e3
@@ -48,7 +67,7 @@ def energy_deg(E,isotropic,dens):
     D_en = gradient(E)
     r = Pat_range(E,isotropic)
 
-    hi = zetm/r[:,None]
+    hi = zetm / r[:,None]
 
     Lambda = lambda_comp(hi,E,isotropic)
 
@@ -57,14 +76,14 @@ def energy_deg(E,isotropic,dens):
 #    for i in range(N_alt0):
 #        Am[:,i] = atmp.iat[i,-1] * Lambda[:,i] * E[:,None] * (1-alb)/r
 
-    Am[0,:] *= D_en[0]/2
-    Am[-1,:]*= D_en[-1]/2
-    Am[1:-2,:] *= (D_en[1:-2]+D_en[0:-3])[:,None]/2
+    Am[0,:] *= D_en[0]/2.
+    Am[-1,:]*= D_en[-1]/2.
+    Am[1:-2,:] *= (D_en[1:-2]+D_en[0:-3])[:,None]/2.
     return Am
 
 def Pat_range(E,isotropic):
-    pr= 1.64e-06 if isotropic else 2.16e-06
-    return pr * (E/1000)**1.67 * (1 + 9.48e-02 * E**-1.57)
+    pr= 1.64e-6 if isotropic else 2.16e-6
+    return pr * (E/1e3)**1.67 * (1 + 9.48e-2 * E**-1.57)
 
 def albedo(E,isotropic):
     isotropic = int(isotropic)
@@ -73,6 +92,7 @@ def albedo(E,isotropic):
       [[0.352, 0.344, 0.334, 0.320, 0.300, 0.280, 0.260, 0.238, 0.218, 0.198, 0.180, 0.160, 0.143, 0.127, 0.119, 0.113, 0.108, 0.104, 0.102, 0.101, 0.100],
        [0.500, 0.492, 0.484, 0.473, 0.463, 0.453, 0.443, 0.433, 0.423, 0.413, 0.403, 0.395, 0.388, 0.379, 0.378, 0.377, 0.377, 0.377, 0.377, 0.377, 0.377]])
     logE=log10(E)
+
     falb=interp1d(logE_p,Param[isotropic,:],kind='linear',bounds_error=False,fill_value=nan)
     alb = falb(logE)
     alb[logE>logE_p[-1]] = Param[isotropic,-1]
@@ -80,19 +100,29 @@ def albedo(E,isotropic):
     return alb
 
 def lambda_comp(hi,E,isotropic):
+    """
+    interpolated over energies from 48.9 eV to 5012 eV
+    for isotropic and field-aligned precipitation
+    """
+#%% field-aligned
     logE_m= append(1.69,arange(1.8,3.7+0.1,0.1))
     Param_m =array(
         [[1.43,1.51,1.58,1.62,1.51,1.54,1.18,1.02, 0.85, 0.69, 0.52,0.35,0.21,0.104,0.065,0.05,0.04,0.03,0.03, 0.025,0.021],
          [0.83,0.77,0.72,0.67,0.63,0.59,0.56,0.525,0.495,0.465,0.44,0.42,0.40,0.386,0.37, 0.36,0.35,0.34,0.335,0.325,0.32],
          [-0.025,-0.030, -0.040, -0.067, -0.105, -0.155, -0.210, -0.275, -0.36, -0.445, -0.51,-0.61, -0.69, -0.77, -0.83, -0.865, -0.90,-0.92, -0.935, -0.958, -0.96],
-         [-1.67,-1.65,-1.62,-1.56,-1.46,-1.35,-1.20,-0.98,-0.70,-0.37,-0.063,0.39,0.62,0.92,1.11,1.25,1.36,1.44,1.50,1.55,1.56]])
-
+         [-1.67,-1.65,-1.62,-1.56,-1.46,-1.35,-1.20,-0.98,-0.70,-0.37,-0.063,0.39,0.62,0.92,1.11,1.25,1.36,1.44,1.50,1.55,1.56]]
+         )
+#%% isotropic
+    """
+        interpolated over energies from 48.9 eV to 1000 eV
+    """
     logE_i=append(1.69, arange(1.8,3.0+0.1,0.1))
     Param_i =array(
         [[0.041, 0.051, 0.0615, 0.071, 0.081, 0.09, 0.099, 0.1075, 0.116, 0.113, 0.13, 0.136, 0.139, 0.142],
          [1.07, 1.01, 0.965, 0.9, 0.845, 0.805, 0.77, 0.735, 0.71, 0.69, 0.67, 0.665, 0.66, 0.657],
          [-0.064, -0.1, -0.132, -0.171, -0.2, -0.221, -0.238, -0.252, -0.261, -0.267, -0.271, -0.274, -0.276, -0.277],
-         [-1.054, -0.95, -0.845, -0.72, -0.63, -0.54, -0.475, -0.425, -0.38, -0.345, -0.319, -0.295, -0.28, -0.268]])
+         [-1.054, -0.95, -0.845, -0.72, -0.63, -0.54, -0.475, -0.425, -0.38, -0.345, -0.319, -0.295, -0.28, -0.268]]
+         )
 
     logE=log10(E)
 
@@ -104,19 +134,89 @@ def lambda_comp(hi,E,isotropic):
         P = Param_m
         LE = logE_m
         Emax = 5000.
-
+#%% interpolate
     fC=interp1d(LE,P,kind='linear',axis=1,bounds_error=False,fill_value=nan)
     C = fC(logE)
-
+#%% low energy
     lam = ((C[0,:][:,None]*hi + C[1,:][:,None]) *
             exp(C[2,:][:,None]*hi**2 + C[3,:][:,None]*hi))
-
+#%% high energy
     badind = E>Emax
-    lam[badind] = ((P[0,-1]*hi[badind] + P[1,-1]) *
-            exp(P[2,-1]*hi[badind]**2 +P[3,-1]*hi[badind]))
+    lam[badind] = (
+                   (P[0,-1]*hi[badind] + P[1,-1]) *
+                   exp(P[2,-1]*hi[badind]**2 + P[3,-1]*hi[badind])
+                   )
+
     return lam
 
 def partition(dens,ki):
-    k = array([ki[0],ki[2],ki[1]])
-    P = k*dens[['N2','O','O2']].values
+    P = ki[[0,2,1]]*dens[['N2','O','O2']].values
     return P / P.sum(axis=1)[:,None]
+
+def reesiono(dtime,altkm,E,glat,glon,f107a,f107,ap,mass,isotropic):
+    if isotropic:
+        print('isotropic pitch angle flux')
+    else:
+        print('field-aligned pitch angle flux')
+
+    tselecopts = array([1,1,1,1,1,1,1,1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],float)
+    dens,temp = rungtd1d(dtime,altkm,glat,glon,f107a,f107,ap,mass,tselecopts)
+#%% temporary for testing with octave
+#    from scipy.io import savemat
+#    savemat('debug_msis.mat',{'z':altkm,'E':E,'O':dens['O'].values,
+#                              'N2':dens['N2'].values,'O2':dens['O2'].values,
+#                              'Total':dens['Total'].values},
+#            oned_as='column')
+#%% python port
+    return ionization_profile_from_flux(E,dens,isotropic)
+
+def plotA(A,z,E,ttxt,vlim):
+    def _doax(ax):
+       ax.yaxis.set_major_locator(MultipleLocator(100))
+       ax.yaxis.set_minor_locator(MultipleLocator(20))
+       ax.set_xscale('log')
+       ax.set_ylabel('altitude [km]')
+       ax.set_title(ttxt)
+
+    fg = figure()
+    ax = fg.gca()
+    hi = ax.pcolormesh(E,z,A,
+                       vmin=vlim[0],vmax=vlim[1],
+                       norm=LogNorm())
+    c=fg.colorbar(hi,ax=ax)
+    c.set_label('Energy Deposition')
+    ax.set_xlabel('beam energy [eV]')
+    ax.autoscale(True,tight=True) #fill axes
+    _doax(ax)
+#%% same data, differnt plot
+    ax = figure().gca()
+    ax.plot(A,z)
+    ax.set_xlim(vlim)
+    ax.set_xlabel('Energy Deposition')
+    _doax(ax)
+
+def loadaltenergrid(minalt=90,Nalt=286,special_grid=''):
+    """
+    makes a tanh-spaced grid (see setupz for info)
+
+    minalt: [km] minimum altiude in grid (e.g. 90)
+    Nalt: number of points in grid
+    special_grid: use same grid as 'transcar' or 'glow'
+    """
+    assert isinstance(special_grid,string_types)
+    #%% altitude
+    if special_grid.lower()=='transcar':
+        z = setupz(286,90,1.5,11.1475)
+    elif special_grid.lower()=='glow':
+        z = glowalt()
+    else:
+        z = setupz(Nalt,minalt,1.5,11.1475)
+
+    z = z[z <= 1000] #keeps original spacing, but only auroral altitudes
+#%% energy of beams
+    if special_grid.lower()=='transcar':
+        E = logspace(1.72,4.25,num=33,base=10)
+    else:
+        E = logspace(1.72,6.,num=81,base=10)
+
+    return z,E
