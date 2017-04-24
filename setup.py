8b8ec8490561f3046ea57d961c0d96210d7bb8a1
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-24 05:12:43 -0400

    prerqe

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setup(name='reesaurora',
       'Development Status :: 4 - Beta',
       'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
       'Topic :: Scientific/Engineering :: Atmospheric Science',
-      'Programming Language :: Python :: 3.6',],
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python',],
 	  )
